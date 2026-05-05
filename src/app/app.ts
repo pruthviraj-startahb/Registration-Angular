d61b4d6e3b812d4657e8a32b4ca6b8d3b37521dd
Pruthviraj-Patil-1434 <pruthvirajpatil1434@gamil.com>
2026-05-05 15:15:11 +0530

    Registration App Using Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,30 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'register',
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('registerApp');
+  fname: string = '';
+  lname: string = '';
+  email: string = '';
+  age: number = 0;
+  clg: string = '';
+  dept: string = '';
+
+  onSubmit() {
+    alert(`
+         ${this.fname} \n
+         ${this.lname} \n
+         ${this.email} \n
+         ${this.age} \n
+         ${this.clg} \n
+         ${this.dept} \n
+         `);
+  }
 }
